9d8b13dbea4b1b92188506fccf34389b6a2767e6
Adam McCullough <merlinfmct87@gmail.com>
2020-02-29 02:23:08 -0800

    A bunch of lint refactorings.

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,19 +4,20 @@ module Lib
     ( getChildrenOp
     ) where
 
-import           Control.Monad         (filterM, join, mapM)
+import           Control.Monad         (filterM, mapM)
 import           Data.ByteString       ()
 import qualified Data.ByteString       as B
 import           Data.ByteString.Char8 (lines, unpack)
 import           Data.List             (foldl')
 import qualified Data.Map              as M
 import qualified Data.Set              as S
-import           Prelude               (Bool (..), IO, Int, Maybe (..), String,
-                                        filter, fmap, head, read, return, show,
-                                        ($), (++), (>>=))
+import           Prelude               (Bool (False, True), IO, Int,
+                                        Maybe (Just, Nothing), String, filter,
+                                        fmap, head, read, return, show, ($),
+                                        (++), (>>=), (=<<))
 import           System.Directory      (doesDirectoryExist, doesFileExist,
                                         listDirectory)
-import           System.IO             (IOMode (..), withFile)
+import           System.IO             (IOMode (ReadMode), withFile)
 import           Text.Regex            (Regex, matchRegex, mkRegex)
 import           Text.Regex.Base       ()
 
@@ -32,10 +33,10 @@ appendMatchSubstrToList :: Regex -> [Int] -> String -> [Int]
 appendMatchSubstrToList regex list str =
   case matchRegex regex str of
     Nothing           -> list
-    Just subexprMatch -> (list ++ [(read (head subexprMatch) :: Int)])
+    Just subexprMatch -> list ++ [read (head subexprMatch) :: Int]
 
 getPidInts :: [Int] -> String -> [Int]
-getPidInts list pidstr = appendMatchSubstrToList pidRegex list pidstr
+getPidInts = appendMatchSubstrToList pidRegex
 
 getPIDPaths :: IO [Int]
 getPIDPaths = do
@@ -59,33 +60,33 @@ getPIDParent pid = do
   if statsFileExists then do
     contents <- withFile statsPath ReadMode B.hGetContents
     let contentLines = lines contents
-        statsContents = foldl' (getPPidInts) [] contentLines
+        statsContents = foldl' getPPidInts [] contentLines
     return $ Just (pid, head statsContents)
   else
     return Nothing
 
 getParentPidList :: IO [Maybe (Int, Int)]
-getParentPidList = getPIDPaths >>= (\a -> mapM getPIDParent a)
+getParentPidList = getPIDPaths >>= mapM getPIDParent
 
 buildPIDChildrenMap :: [Maybe (Int, Int)] -> M.Map Int (S.Set Int)
-buildPIDChildrenMap list =
+buildPIDChildrenMap =
   foldl' (\map maybePair ->
     case maybePair of
       Nothing -> map
       Just (pid, pidParent) -> M.insertWith
-        (S.union)
+        S.union
        pidParent
        (S.singleton pid)
        map
-  ) (M.fromList []) list
+  ) (M.fromList [])
 
-getChildren :: (M.Map Int (S.Set Int)) -> Int -> [Int]
+getChildren :: M.Map Int (S.Set Int) -> Int -> [Int]
 getChildren map pid = do
   let childrenSetMaybe = M.lookup pid map
   case childrenSetMaybe of
     Nothing  -> []
     Just set -> let list = S.toList set in
-        list ++ (join $ fmap (getChildren map) list)
+        list ++ (getChildren map =<< list)
 
 getChildrenOp :: Int -> IO [Int]
 getChildrenOp pidRoot = do
@@ -93,6 +94,3 @@ getChildrenOp pidRoot = do
   let map = buildPIDChildrenMap ppid_list
       pidList = getChildren map pidRoot
   return pidList
-
--- pidPaths <- getPIDPaths
--- pidParentPairs <- mapM getPIDParent pidPaths
